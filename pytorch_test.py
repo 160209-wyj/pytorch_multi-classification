3ec6431b725fb0853e610ee8643eb83a577ac767
160209-wyj <68934822+160209-wyj@users.noreply.github.com>
2020-08-19 11:46:29 +0800

    Update pytorch_test.py

--- pytorch_test.py
+++ pytorch_test.py
@@ -11,6 +11,50 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
+class GCNResnet_junjie(nn.Module):
+    def __init__(self, model, num_classes, in_channel=300, t=0, adj_file='data/voc/voc_adj.pkl'):
+        super(GCNResnet_junjie, self).__init__()
+        self.features = nn.Sequential(
+            model.conv1,
+            model.bn1,
+            model.relu,
+            model.maxpool,
+            model.layer1,
+            model.layer2,
+            model.layer3,
+            model.layer4,
+        )
+        self.num_classes = num_classes
+        # self.pooling = nn.MaxPool2d(14, 14)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(2048, num_classes)
+        self.relu = nn.LeakyReLU(0.2)
+
+        # image normalization
+        self.image_normalization_mean = [0.485, 0.456, 0.406]
+        self.image_normalization_std = [0.229, 0.224, 0.225]
+
+    def forward(self, feature):
+        feature = self.features(feature)
+        feature = self.avgpool(feature)
+        feature = feature.view(feature.size(0), -1)
+        feature= self.fc(feature)
+        return feature
+
+    def get_config_optim(self, lr, lrp):
+        return [
+                {'params': self.features.parameters(), 'lr': lr * lrp},
+                {'params': self.fc.parameters(), 'lr': lr},
+                # {'params': self.gc2.parameters(), 'lr': lr},
+                ]
+
+
+
+
+def gcn_resnet50(num_classes, pretrained=True):
+    model = models.resnet50(pretrained=pretrained)
+    return GCNResnet_junjie(model, num_classes)
+
 class vgg_junjie(nn.Module):
     def __init__(self,num_classes=20,init_weights=True):
         super(vgg_junjie,self).__init__()
@@ -82,9 +126,27 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
  
 # ------------------------ 载入模型并且训练 --------------------------- #
 
-model = torch.load(model_path, map_location='cuda:0')
-model.eval()
+# model = torch.load(model_path, map_location='cuda:0')
+# model.eval()
 # print(model)
+#加载最终模型
+# model = torch.load(model_path, map_location='cuda:0')
+# model.eval()
+
+#加载中间模型resnet50
+num_classes = 7
+model = gcn_resnet50(num_classes=num_classes,pretrained=True)
+model.cuda()
+model.load_state_dict(torch.load(model_path))
+model.eval()
+
+#加载中间模型vgg16_bn
+# num_classes = 7
+# model = vgg_junjie(num_classes=num_classes)
+# model.cuda()
+# model = nn.DataParallel(model)
+# model.load_state_dict(torch.load(model_path))
+# model.eval()
 
 dire = '/home/zhangjunjie/keras-applications-master/kafka_data/all_test_data/'
 # dire = '/home/zhangjunjie/MobileNetV2-master/image_140_yuanchicun/images_highrisk/'
